2f65960bde3c16152360a8e9487ab309e8351652
YuriCJardim <yuricjardim@gmail.com>
2022-10-21 17:36:39 -0300

    test: criando novo teste

--- src/main/java/com/yuricjardim/uploadingfiles/UploadingFilesApplication.java
+++ src/main/java/com/yuricjardim/uploadingfiles/UploadingFilesApplication.java
@@ -1,13 +1,27 @@
 package com.yuricjardim.uploadingfiles;
 
+import com.yuricjardim.uploadingfiles.storage.StorageProperties;
+import com.yuricjardim.uploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadingFilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UploadingFilesApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner init (StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+	}
 
-}
+
